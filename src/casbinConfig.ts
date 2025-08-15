2785289efbd03c5c613cbe3b3b1f5c176ee04b1c
taizod1024 <taizod1024@gmail.com>
2025-08-15 11:49:34 +0000

    feat: 不要なCasbin設定ファイルとリソースファイルを削除

--- src/casbinConfig.ts
+++
@@ -1,7 +0,0 @@
-import { newEnforcer } from "casbin";
-
-export async function getEnforcer() {
-  // サンプル: ファイルベースのモデルとポリシー
-  const enforcer = await newEnforcer("./src/rbac_model.conf", "./src/rbac_policy.csv");
-  return enforcer;
-}
